fac8d0e12bbde49e1fc18fff9e448b64bd535b7d
Rafael Silverio <fael_st@hotmail.com>
2022-12-04 23:10:02 -0300

    adjust create company dto

--- src/infrastructure/controllers/companies/companies.dto.ts
+++ src/infrastructure/controllers/companies/companies.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNotEmpty, isString, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCompanyDto {
   @ApiProperty()
